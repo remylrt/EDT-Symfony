e6b46306ea8c144791a7a9c73fc764443f139a8e
Remy Lartiguelongue <remy.lartiguelongue@gmail.com>
2021-03-10 17:15:42 +0100

    Ajout d'une contrainte sur le numéro de la salle pour éviter les doublons.

--- src/Entity/Salle.php
+++ src/Entity/Salle.php
@@ -9,9 +9,14 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=SalleRepository::class)
+ * @UniqueEntity(
+ *      fields={"numero"},
+ *      message="Cette salle existe déjà."
+ * )
  */
 class Salle implements JsonSerializable {
     /**
