628800a73ab4d7bb8700fb428db20570316df643
nicolaslps <ybozze150@gmail.com>
2021-03-10 13:31:50 +0100

    base page d'accueil

---
+++ public/js/home.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    delimiters: ['${', '}'],
+    el: '#app',
+    vuetify: new Vuetify({
+        lang: {
+            current: 'fr'
+        },
+    }),
+    data: {
+
+    },
+    methods: {
+
+    },
+})
