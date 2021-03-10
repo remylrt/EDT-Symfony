b41098c1def452fbd65a08b14685d55815ca8d06
nicolaslps <ybozze150@gmail.com>
2021-03-10 15:54:00 +0100

    affichage des nouveaux articles avec des images avec le flux rss de l'IUT

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,38 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
 
-        ]);
+
+    //Read each feed's items
+    $xml = simplexml_load_file("https://www.iutbayonne.univ-pau.fr/rss/news");
+    $articles = $xml->xpath("//item");
+
+    foreach ($articles as $key => $article) {
+        $htmlContent = $article->description;
+
+        //récupération image
+        preg_match('/<img typeof="foaf:Image" class="img-responsive" src="(.*?)" width="100" height="100" alt="" \/>/s', $htmlContent, $matchImage);
+        if(array_key_exists(1, $matchImage)){
+            $article->image = $matchImage[1];
+        }
+
+        //récupération description
+        preg_match('/<p>(.*?)<\/p>/s', $htmlContent, $matchDescription);
+        if(array_key_exists(1, $matchDescription)){
+            $article->description = $matchDescription[1];
+        }
+
+        if(sizeof($matchImage) == 0){
+            unset($articles[$key]);
+        }
+    }
+
+
+
+
+
+
+
+    return $this->render('home/index.html.twig', [ "articles" => $articles ]);
     }
 }
