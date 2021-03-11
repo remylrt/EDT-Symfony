5a6b7320182e88cf4de9045e0424c916be98bb43
nicolaslps <ybozze150@gmail.com>
2021-03-11 11:18:17 +0100

    modification du formulaire de connexion

---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    delimiters: ['${', '}'],
+    el: '#app',
+    vuetify: new Vuetify({
+        lang: {
+            current: 'fr'
+        },
+    }),
+    data: {
+    },
+    methods: {
+    },
+})
